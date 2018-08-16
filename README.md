708c60fc7531d076545f1a772e674e7949b48f7e
juan-pablo-vielma <jvielma@mit.edu>
2018-08-16 12:18:14 -0400

    Add autogenerated files to build ECOSBuilder

---
+++ README.md
@@ -0,0 +1,9 @@
+# ECOSBuilder
+
+[![Build Status](https://travis-ci.org/juan-pablo-vielma/ECOSBuilder.svg?branch=master)](https://travis-ci.org/juan-pablo-vielma/ECOSBuilder)
+
+This repository builds binary artifacts for the ECOSBuilder project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/juan-pablo-vielma/ECOSBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,54 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+# Collection of sources required to build ECOSBuilder
+sources = [
+    "https://github.com/embotech/ecos/archive/v2.0.6.tar.gz" =>
+    "ffc4b44e813a5c68abd464b638d216f09385d9b6e40cf487ac384289e6a27988",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd ecos-2.0.6/
+make shared
+mkdir $prefix/lib
+cp libecos.so $prefix/lib
+cp libecos.dll $prefix/lib
+cp libecos.dylib $prefix/lib
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, :glibc),
+    Linux(:x86_64, :glibc),
+    Linux(:aarch64, :glibc),
+    Linux(:armv7l, :glibc, :eabihf),
+    Linux(:powerpc64le, :glibc),
+    Linux(:i686, :musl),
+    Linux(:x86_64, :musl),
+    Linux(:aarch64, :musl),
+    Linux(:armv7l, :musl, :eabihf),
+    MacOS(:x86_64),
+    FreeBSD(:x86_64),
+    Windows(:i686),
+    Windows(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libecos", :libecos)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "ECOSBuilder", sources, script, platforms, products, dependencies)
+
